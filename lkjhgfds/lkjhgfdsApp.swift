f6253db971ddf7f1ad5c4d513a21305f4fc7bdea
najla <alabdullahnajla3@gmail.com>
2024-05-01 07:13:44 -0700

    Initial Commit

---
+++ lkjhgfds/lkjhgfdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  lkjhgfdsApp.swift
+//  lkjhgfds
+//
+//  Created by mac on 22/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct lkjhgfdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
